f6035f482c049f322025675f203dc77fb48dc26b
Jimmy Nguyen <kientnguyen@hotmal.com>
2022-01-27 09:45:50 +0100

    Adjusted e2e test

--- cypress/integration/1-homepage/homepage.spec.ts
+++ cypress/integration/1-homepage/homepage.spec.ts
@@ -7,7 +7,7 @@ describe("Homepage", () => {
 
     it("should display hero section with a small introduction.", () => {
         cy.get(".hero")
-            .should('have.text', 'Hi! My name is Jimmy Nguyen I am a Freelance Software Engineer living in the Netherlands who likes to create fully-functional and maintainable software solutions. Currently doing a project at DPG Media. Available for work in March 2022!')
+            .should('have.text', 'Hi! My name is Jimmy Nguyen I am a Freelance Software Engineer living in the Netherlands who likes to create fully-functional and maintainable software solutions. Currently doing a project at DPG Media. Currently not available for work.')
         cy.get(".availability").should("exist")
     })
 
@@ -19,7 +19,7 @@ describe("Homepage", () => {
 
         cy.get(".about-text").should("exist")
             .should("contain.text", `Hello! I am a Software Engineer who has a lot of experience in creating quality software for different kinds of businesses!Full-stack development? I got you! Need some help with migrations to the Cloud?I'm your guy! Need a small web application or a huge enterprise application? I can help :)`)
-        
+
         cy.get(".about-buttons").get('#short-version-button').click({force: true})
 
         cy.get(".about-text").should("contain.text", "I do cool stuff with computers and code.")
