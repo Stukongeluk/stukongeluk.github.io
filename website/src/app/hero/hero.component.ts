1b8979e762896012d2114fcffc1c902fee6e49e0
StukOngeluk <kientnguyen@hotmail.com>
2025-04-01 10:56:15 +0200

    chore: small code refactor and fixed some smol issue

--- website/src/app/hero/hero.component.ts
+++ website/src/app/hero/hero.component.ts
@@ -22,33 +22,27 @@ export class HeroComponent implements OnInit {
 
   public updateDisplayRole() {
     this.currentWord = this.roles[this.roleIndex];
-    if (this.isDeleting) {
-      this.displayedRole = this.currentWord.substring(0, this.currentWordLength-1);
-      this.currentWordLength--;
-      if (this.currentWordLength == 0) {
+
+    this.displayedRole = this.isDeleting
+        ? this.currentWord.substring(0, --this.currentWordLength)
+        : this.currentWord.substring(0, ++this.currentWordLength);
+
+    if (this.currentWordLength === 0) {
         this.isDeleting = false;
-        this.roleIndex++;
-        if (this.roleIndex == this.roles.length) {
-          this.roleIndex = 0;
-        }
-      }
-    } else {
-      this.displayedRole = this.currentWord.substring(0, this.currentWordLength+1);
-      this.currentWordLength++;
-      if (this.currentWordLength == this.currentWord.length) {
+        this.incrementRoleIndex();
+    } else if (this.currentWordLength === this.currentWord.length) {
         this.isDeleting = true;
-      }
     }
-    this.scheduleUpdateDisplayedRole()
+
+    this.scheduleUpdateDisplayedRole();
+  }
+
+  private incrementRoleIndex() {
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
   }
 
   private scheduleUpdateDisplayedRole() {
-    const delayBetweenWords = 1000;
-    const delayBetweenLetters = 100;
-    if (this.currentWordLength == this.currentWord.length) {
-      setTimeout(() => this.updateDisplayRole(), delayBetweenWords)
-    } else {
-      setTimeout(() => this.updateDisplayRole(), delayBetweenLetters)
-    }
+      const delay = this.currentWordLength === this.currentWord.length ? 1000 : 100;
+      setTimeout(() => this.updateDisplayRole(), delay);
   }
 }
