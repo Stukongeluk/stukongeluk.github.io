f31f915b1048a648905bd467b3ca2fea8cd1f5b9
StukOngeluk <kientnguyen@hotmail.com>
2025-12-30 12:00:04 +0100

    Optimizations and new hero image

--- website/src/app/hero/hero.component.ts
+++ website/src/app/hero/hero.component.ts
@@ -1,10 +1,9 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
@@ -17,32 +16,32 @@ export class HeroComponent implements OnInit {
   isDeleting: boolean = false;
 
   ngOnInit(): void {
-    this.updateDisplayRole()
+    this.updateDisplayRole();
   }
 
   public updateDisplayRole() {
     this.currentWord = this.roles[this.roleIndex];
 
     this.displayedRole = this.isDeleting
-        ? this.currentWord.substring(0, --this.currentWordLength)
-        : this.currentWord.substring(0, ++this.currentWordLength);
+      ? this.currentWord.substring(0, --this.currentWordLength)
+      : this.currentWord.substring(0, ++this.currentWordLength);
 
     if (this.currentWordLength === 0) {
-        this.isDeleting = false;
-        this.incrementRoleIndex();
+      this.isDeleting = false;
+      this.incrementRoleIndex();
     } else if (this.currentWordLength === this.currentWord.length) {
-        this.isDeleting = true;
+      this.isDeleting = true;
     }
 
     this.scheduleUpdateDisplayedRole();
   }
 
   private incrementRoleIndex() {
-      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+    this.roleIndex = (this.roleIndex + 1) % this.roles.length;
   }
 
   private scheduleUpdateDisplayedRole() {
-      const delay = this.currentWordLength === this.currentWord.length ? 1000 : 100;
-      setTimeout(() => this.updateDisplayRole(), delay);
+    const delay = this.currentWordLength === this.currentWord.length ? 1000 : 100;
+    setTimeout(() => this.updateDisplayRole(), delay);
   }
 }
