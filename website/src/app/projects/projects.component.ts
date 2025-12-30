fe7d8630041796be1524cc0bd8ae15548966143a
StukOngeluk <kientnguyen@hotmail.com>
2025-12-30 16:56:47 +0100

    Some smol changes again

--- website/src/app/projects/projects.component.ts
+++ website/src/app/projects/projects.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Project {
+  title: string;
+  description: string;
+  image?: string;
+  link?: string;
+  tags: string[];
+  featured?: boolean;
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent { }
+export class ProjectsComponent {
+  projects = signal<Project[]>([]);
+}
